5fd033465d19cab09baac27a235dfeccdb0ed394
Lincoln Keesecker <lekeesecker@gmail.com>
2023-03-01 10:41:08 -0600

    finished phrases in database

--- scripts/database.js
+++ scripts/database.js
@@ -2,51 +2,51 @@ database = [
     {
         id: 1,
         image: "https://www.rd.com/wp-content/uploads/2018/10/Creepy-clown.jpg?resize=768,512",
-        phrase: 
+        phrase: "Ew"
     },
     {
         id: 2,
         image: "https://thumbs.dreamstime.com/b/bug-coming-human-nose-20246031.jpg",
-        phrase: 
+        phrase: "That's Nasty"
         },
     {
         id: 3,
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkmtdHJ-Ap2BPQw2AW3dFdmaRV8ZLI79BgTA&usqp=CAU",
-        phrase: 
+        phrase: "Why would you click this?"
     },
     {
         id: 4,
         image: "https://www.irishtimes.com/resizer/s_iF7Qn4Y4Q31byqM3fqHWrPYw4=/1600x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/irishtimes/SQYVNYALPWPZVCAN4AGAY6PRAY.jpg",
-        phrase: 
+        phrase: "Who hurt you?"
     },
     {
         id: 5,
         image: "https://t4.ftcdn.net/jpg/03/08/56/63/240_F_308566393_31OWda4VhrEP0glhbzv4OBW8t6U35Ylz.jpg",
-        phrase: 
+        phrase: "🤢🤢🤢"
     },
     {
         id: 6,
         image: "https://scontent-atl3-2.xx.fbcdn.net/v/t31.18172-8/12841200_10153743712113429_3466610789161922118_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=9267fe&_nc_ohc=y9DtgTHVGfoAX_CKRr_&_nc_ht=scontent-atl3-2.xx&oh=00_AfDL-CMwW5ncctKvYata7R_X8WGqJ6sM8R1pNc0ai_gs1A&oe=64270043",
-        phrase: 
+        phrase: "I'm not mad, I'm disappointed"
     },
     {
         id: 7,
         image: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Braised_duck_teochew.jpg",
-        phrase: 
+        phrase: "You're making Conor sad..."
     },
     {
         id: 8,
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Korean.cuisine-Sannakji.hoe-01.jpg/300px-Korean.cuisine-Sannakji.hoe-01.jpg",
-        phrase: 
+        phrase: "👻 Martin's Aquarium 👻"
     },
     {
         id: 9,
         image: "https://media.kohlsimg.com/is/image/kohls/3338223?wid=1200&hei=1200&op_sharpen=1",
-        phrase: 
+        phrase: "You forgot a comma"
     },
     {
         id: 10,
         image: "https://www.shutterstock.com/image-photo/beauty-female-dark-red-squeezing-600w-215885275.jpg",
-        phrase: 
+        phrase: "Look behind you!"
     }
 ]
