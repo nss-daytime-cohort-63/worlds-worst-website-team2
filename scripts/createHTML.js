08c28e54005fa20de994434fe016a58551a0a0ed
Conor Getty <conorgetty@gmail.com>
2023-03-01 11:05:01 -0600

    changed HTML again

--- scripts/createHTML.js
+++ scripts/createHTML.js
@@ -1,22 +1,17 @@
-export const FishList = () => {
+export const databasePopups = () => {
     // Invoke the function that you imported from the database module
-    const fishes = getFish()
+    const database = getDatabase()
 
     // Start building a string filled with HTML syntax
-    let htmlString = '<article class="fishList">'
+    let htmlString = ""
 
     // Create HTNL representations of each fish here
     for (const fish of fishes) {
 
         // Why is there a backtick used for this string?
-        htmlString += `<section class="fishCard">
-            <div><img  class="fishImage image--card" src="${fish.image}" alt="blue fish"/></div>
-            <div class="fishName">${fish.name}</div>
-            <div class="fishSpecies">${fish.species}</div>
-            <div class="fishLength">${fish.length}</div>
-            <div class="fishLocation">${fish.location}</div>
-            <div class="fishDiet">${fish.food}</div>
-        </section>
+        htmlString += `<div class="popup" onclick="myFunction()">Click me!
+        <span class="popuptext" id="myPopup">Popup text...</span>
+                    </div>
 `
     }
     htmlString += `</article>`
